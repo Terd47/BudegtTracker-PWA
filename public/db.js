89627e38a7da2f17cd3a82788ca8f355ea79e5d5
Terd47 <terdlar@gmail.com>
2021-01-11 01:35:46 -0500

    updated service worker to fixed 404 icons

--- public/db.js
+++ public/db.js
@@ -4,7 +4,7 @@ const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
    // create object store 
-  const db = event.target.result;
+  db = event.target.result;
   db.createObjectStore("pendingTransactions", { autoIncrement: true });
 };
 
@@ -17,27 +17,20 @@ request.onsuccess = function(event) {
 };
 
 request.onerror = function(event) {
-  console.log("Woops! " + event.target.errorCode);
+  db = event.target.result;
 };
 
 function saveRecord(record) {
   // create a transaction on the pending db with readwrite access
   const transaction = db.transaction(["pendingTransactions"], "readwrite");
-
-  // access your pending object store
-  const store = transaction.objectStore("pendingTransactions");
-
-  // add record to your store with add method.
-  store.add(record);
+  const pendingTransactions = transaction.objectStore("pendingTransactions");
+  pendingTransactions.add(record);
 }
 
 function checkDatabase() {
-  // open a transaction on your pending db
   const transaction = db.transaction(["pendingTransactions"], "readwrite");
-  // access your pending object store
-  const store = transaction.objectStore("pendingTransactions");
-  // get all records from store and set to a variable
-  const getAll = store.getAll();
+  const pendingTransactions = transaction.objectStore("pendingTransactions");
+  const getAll = pendingTransactions.getAll();
 
   getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
@@ -51,18 +44,14 @@ function checkDatabase() {
       })
       .then(response => response.json())
       .then(() => {
-        // if successful, open a transaction on your pending db
         const transaction = db.transaction(["pendingTransactions"], "readwrite");
-
         // access your pending object store
-        const store = transaction.objectStore("pendingTransactions");
-
-        // clear all items in your store
-        store.clear();
+        const pendingTransactions = transaction.objectStore("pendingTransactions");
+        db = request.result;
+        pendingTransactions.clear();
       });
     }
   };
 }
-
 // listen for app coming back online
 window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,61 +1,70 @@
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+// PWA service worker to cache data
+const APP_CACHE = 'static-cache-v2';
+const DATA_CACHE = 'data-cache-v1';
 const FILES_TO_CACHE = [
-  '/index.html',
+  '/',
   '/db.js',
+  '/index.html',
   '/styles.css',
   '/index.js',
-  '/manifest.json',
+  '/manifest.webmanifest',
   '/icons/icon-192x192.png',
-  '//icons/icon-512x512.png',
-  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
+  '/icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
+    caches.open(APP_CACHE)
+      .then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
   );
+    self.skipWaiting();
 });
 
 // The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== APP_CACHE && key !== DATA_CACHE) {
+            console.log("Remove old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // on successful response, clone and store in cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
+            return response;
+          })
+          .catch(err => {
+            // if no network, check cache
+            return cache.match(evt.request);
           });
-        });
-      })
+      }).catch(err => console.log(err))
     );
+
+    return;
   }
+  evt.respondWith(
+    caches.match(evt.request).then(function(response) {
+      return response || fetch(evt.request);
+    })
+  );
 });
